9d916da55dd655c106f68a3ac468004f7a0f0f40
Yannick Couillin <Inforaz-admin@INFORAZ.local>
2020-03-30 10:49:00 +0200

    modification

--- index.php
+++ index.php
@@ -21,22 +21,19 @@ class Personnage {
     public $nom;
     public $force;
     public $niveau;
-    public $santé;
+    public $sante;
     public $mort;
 
 
-    function 
-    if($mort < 0,){
-        echo "il est mort";
-    }elseif($mort >= 0{
-        echo "il est vivant";
-    }
-
-
     public function caracteristique(){
-        echo $this => nom ." a une force de" . $this =>force . "est de niveau1" . $this => niveau1  "est en bonne ".this => santé "il est" . this=>mort;
+        echo $this -> nom  ." a une force de". $this ->force ."est de niveau1". $this -> niveau1 ."est en bonne". $this -> santé ."il est". $this->mort ."". 
 
-    }
+    function  mort(){
+    if($this = $mort < 0){
+        $this "il est mort";
+    }elseif($this = $mort > 0){
+        $this "il est vivant";
+    }}
 }
 
 $perso1 = new personnage();
