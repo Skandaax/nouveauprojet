896ee0aa39109b7fb0f5c4801f3bafc45fada4c8
Yannick Couillin <Inforaz-admin@INFORAZ.local>
2020-04-06 14:59:46 +0200

    modification

--- index.php
+++ index.php
@@ -1,145 +1,6 @@
 <?php
 
-abstract class Personnage
-{
-    protected $nom;
-    protected $force;
-    protected $niveau;
-    protected $health;
-    protected $death;
-    protected $fleches;
-
-    function __construct(string $nom, int $force, $niveau = 1, int $health = 100)
-    {
-        $this->setnom($nom);
-        $this->setforce($force);
-        $this->setniveau($niveau);
-        $this->sethealth($health);
-        $this->setdeath();
-    }
-
-    function caracteristiques()  
-    {
-        $etat = ($this->death)? "mort" : "vivant";
-        echo $this->nom ." a une force de ". $this->force." et au niveau ".$this->niveau." est vivant ".$this->health." est mort ".$this->death."ponts/100, notre personnage est donc ".$etat."<br>";
-    }
-
-    /********Nom********/
-    function getNom() : string 
-    {
-        return $this->nom;
-    }
-
-    function setNom(string $nom) 
-    {
-        $this->nom = $nom;
-    }
-
-    /********Force********/
-    function getForce() : string 
-    {
-        return $this->force;
-    }
-    
-    function setForce($force) 
-    {
-        $this->force = $force;
-    }
-
-    /********Niveau********/
-    function getNiveau() : int 
-    {
-        return $this->level;
-    }
-
-    
-    function setNiveau(int $lvl)
-    {
-        $this->niveau = $lvl;
-    }
-
-    /********Vie********/
-    function getHealth() 
-    {
-        return $this->health;
-    }
-
-    function setHealth($health) 
-    {
-        $this->health = $health;
-    }
-    /********Vie ou mort********/
-    function setDeath() 
-    {
-        if($this->health < 1){
-            $this->death = true;
-           } else {
-                $this->death = false;
-           } 
-    }
-
-    /********Attaque********/
-    abstract function attaquer(Personnages $perso);
-
-
-    /********Tirer*********/
-    function tirer()
-    {
-        echo " Le personnage tire une flèche ";
-    }
-        
-    /********Level up********/
-    function levelup()
-    {
-        $this->niveau++;
-    }
-
-    /********Détgats********/
-    function degats($perso)
-    {
-        if ($perso instanceof Archer)
-        {
-            $perso->sethelth($perso->gethealth() - 30);
-        }else 
-        {
-            $perso->sethealth($perso->gethealth() - 100);
-        }
-    }
-
-/*************Les dégats subits */
-    function subirdegats($perso) 
-    {
-        if($this->subirdegats < 10)
-        {
-            $this->subirdegats -= 10;
-        }
-    }
-    
-}
-
-/***********Extension de l'archer********/
-class Arch extends Personnage
-{
-
-    function attaquer($perso)
-    {
-        $this->tirer();
-        if($perso instanceof Archer)
-        {
-            $perso->setdeath($perso->getdeath() - 10);
-        }
-        $perso->setHealth($perso->getHealth() - $this->force);
-        $perso->setDeath();
-    }
-
-
-}
-
-/**************************************************************/
-
-/**************************************************************/
-
-/************Archer********************************************/
+/************Archer************/
 class Archer 
 {
     protected $nom;
@@ -232,6 +93,8 @@ class Archer
         echo " Le personnage tire une flèche ";
     }
         
+    
+
     /********Level up********/
     function levelup()
     {
@@ -275,9 +138,9 @@ class Arche extends Archer
     }
 
     /*************Interface*******************/
+    
 
-
-    /********La force du magicien********/
+    /********La force de l'archer********/
     function robin($perso)
     {
         $perso->recevoirDegats($this->fleches);
@@ -445,12 +308,11 @@ class guerre extends guerrier
             $this->coup += 10;
         }
     }
-
-        function frappe()
+    
+    function lancerunsort() 
     {
-        echo $this->nom." de type ".Guerrier::class." Frappe.<br>";
+        echo $this->nom." de type ".Magicien::class." lance un sort.<br>";
     }
-
 }
 
 
@@ -602,10 +464,6 @@ $perso1 = new Archer("Rose", 12);
 $perso2 = new guerrier("Golbu", 15, 10, 2); 
 $perso3 = new Magicien("Arthis", 13, 2);
 
-$perso4 = new Guerrier("Golbu", 15, 10, 2);
-
-
-
 
 echo "Avant attaque : " ;
 $perso2->caracteristiques();
@@ -618,8 +476,3 @@ $perso1->caracteristiques();
 $perso1->levelup();
 echo "Aprés : ";
 $perso1->caracteristiques();
-
-
-
-
-
